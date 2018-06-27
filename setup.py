e47a212c102cbb2c734095edd0467922907c9d08
Tin Tvrtkovic <tinchester@gmail.com>
2018-06-27 23:15:00 +0200

    Prepare for PyPy.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+from platform import python_implementation
+
 from setuptools import setup, find_packages
-from Cython.Build import cythonize
+
+if python_implementation() != "PyPy":
+    from Cython.Build import cythonize
+
+    ext_modules = cythonize("src/flattr/cflattr/*.pyx", annotate=True)
+else:
+    ext_modules = []
 
 setup(
     name="flattrs",
     version="0.1",
-    install_requires=["attrs", "flatbuffers==20180424161048"],
+    install_requires=["attrs", "flatbuffers==1.9"],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    dependency_links=[
-        "git+https://github.com/google/flatbuffers@v1.9.0#egg=flatbuffers-20180424161048&subdirectory=python"
-    ],
-    ext_modules=cythonize("src/flattr/cflattr/*.pyx", annotate=True),
+    ext_modules=ext_modules,
     zip_safe=False,
     extras_require={"dev": ["pytest", "hypothesis"]},
 )
