3cafea43adaf311a4bfee1692308090b86659fb9
Tin Tvrtkovic <tinchester@gmail.com>
2018-09-21 16:25:58 +0200

    Unions. Infrastructure stuff.

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
+WHEEL_DOCKER_IMAGE := quay.io/pypa/manylinux1_x86_64
+
 fbs:
 	flatc --python -o tests tests/flatbufferdefs/*.fbs
+
+wheels:
+	rm -rf wheelhouse build &&\
+	docker pull $(WHEEL_DOCKER_IMAGE) &&\
+	docker run --rm -v `pwd`:/io $(WHEEL_DOCKER_IMAGE) /io/build-wheels.sh
---
+++ build-wheels.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/cp3{5,6,7}*/bin; do
+    "${PYBIN}/pip" install /io/
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/flattrs*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
