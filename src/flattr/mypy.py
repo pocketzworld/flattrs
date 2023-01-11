9b35cbd1eb6a0d89fb48714dbc0883df3b800b20
Tin Tvrtković <tinchester@gmail.com>
2023-01-12 00:12:11 +0100

    Update mypy plugin

--- src/flattr/mypy.py
+++ src/flattr/mypy.py
@@ -1,9 +1,9 @@
 from mypy.plugin import Plugin
-from mypy.plugins.attrs import attr_class_makers
+from mypy.plugins.attrs import attr_define_makers
 
-# This work just like attr.s i.e. it has the same defaults as attr.s
-attr_class_makers.add("flattr.Flatbuffer")
-attr_class_makers.add("flattr.from_package")
+# This work just like @attrs.define
+attr_define_makers.add("flattr.Flatbuffer")
+attr_define_makers.add("flattr.from_package")
 
 
 class MyPlugin(Plugin):
