c8999917812b4b5b3b2e7f3daa6e441596c6c43d
Tin Tvrtković <tinchester@gmail.com>
2023-01-25 15:25:34 +0100

    More coverage

--- tests/flatc/test_misc.py
+++ tests/flatc/test_misc.py
@@ -12,6 +12,17 @@ def test_enum_name_mismatch() -> None:
     with pytest.raises(ValueError):
 
         @FlatbufferEnum(ASimpleUByteEnum)
+        class ASimpleUbyteEnum(IntEnum):  # Name does't match the flatc version.
+            ONE = 0
+            TWO = 255
+
+
+def test_enum_member_name_mismatch() -> None:
+    from flattrs_test.ASimpleUByteEnum import ASimpleUByteEnum
+
+    with pytest.raises(ValueError):
+
+        @FlatbufferEnum(ASimpleUByteEnum)
         class ASimpleUByteEnum(IntEnum):
             ONE = 0
             TWOO = 255
