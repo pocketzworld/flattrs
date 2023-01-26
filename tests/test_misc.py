9824374c1566fa6a4d203097d5d56fc1bd23056d
Tin Tvrtković <tinchester@gmail.com>
2023-01-26 12:06:20 +0100

    Get rid of nonnestables, improve coverage

---
+++ tests/test_misc.py
@@ -0,0 +1,17 @@
+"""Misc tests for coverage."""
+from enum import Enum
+
+from pytest import raises
+
+from flattr import flattrs
+
+
+def test_unsupported_enums() -> None:
+    class TestEnum(str, Enum):
+        TEST = "test"
+
+    with raises(TypeError):
+
+        @flattrs
+        class Test:
+            test: TestEnum
